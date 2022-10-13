bff54873d12815fd9a4d7c57ac90c73eaa2735cb
Mahfuj Ahbab <ahbab@umich.edu>
2022-10-12 22:42:48 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='bt_view',
+    name='btplus',
     packages=find_packages(),
     version='0.1.0',
     description='Backtester web app built on bt and streamlit',
